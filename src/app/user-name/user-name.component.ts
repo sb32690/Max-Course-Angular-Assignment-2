72dd684076e4a6ed9bc77bd035ec7db54aa16102
sbaxley <sb32690@gmail.com>
2019-05-28 13:06:35 -0400

    first commit

---
+++ src/app/user-name/user-name.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-name',
+  templateUrl: './user-name.component.html',
+  styleUrls: ['./user-name.component.css']
+})
+export class UserNameComponent implements OnInit {
+  userName = '';
+  constructor() { 
+  }
+    
+  ngOnInit() {
+
+  }
+
+}
